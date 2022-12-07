e0a7ed6ecd4657682e8a3ca12ad1d44ed367910f
GilianaB <giliana.b3@yandex.ru>
2022-12-07 23:29:19 +0300

    homework

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿string num = Console.ReadLine();
+int number;
+bool success = int.TryParse(num, out number);
+Console.WriteLine (success + " " + number);
+if (num.Length != 5)
+    {
+    Console.WriteLine("неверный ввод");
+    }
+else
+    if (num[0]==num[4] && num[1]==num[3])
+    {
+    Console.WriteLine("это палиндром");
+    }
+    else
+    {
+    Console.WriteLine("это не палиндром");
+    }
---
+++ task2/Program.cs
@@ -0,0 +1,6 @@
+﻿double abc=0;
+double [] points = new double [6];
+for (int i=0; i < points.Length; i++)
+points[i] = Convert.ToDouble(Console.ReadLine());
+abc=Math.Sqrt((points[3]-points[0])*(points[3]-points[0])+(points[4]-points[1])*(points[4]-points[1])+(points[5]-points[2])*(points[5]-points[2]));
+Console.WriteLine(abc);
